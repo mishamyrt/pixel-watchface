b021d5f3f1971cf3798432340c463353e84b3e3e
mishamyrt <misha@myrt.co>
2022-02-07 15:26:26 +0400

    Add dev script

--- scripts/dev.js
+++ scripts/dev.js
@@ -1,2 +1,24 @@
 // @ts-check
+const nodemon = require('nodemon')
+const { stdout, stderr } = require('process')
 
+const FRONTEND_GLOB = 'src/**/*'
+const BACKEND_GLIB = 'server/**/*'
+const START_CMD = 'npm run build && npm start'
+
+const NODEMON_PARAMS = {
+  exec: START_CMD,
+  watch: [FRONTEND_GLOB, BACKEND_GLIB],
+  ext: 'ts,json,html,scss,svg',
+  stdout: false
+}
+
+// TODO: Add autoreload
+function main () {
+  const process = nodemon(NODEMON_PARAMS)
+  process.on('start', () => console.clear())
+  process.on('stdout', buffer => stdout.write(buffer))
+  process.on('stderr', buffer => stderr.write(buffer))
+}
+
+main()
