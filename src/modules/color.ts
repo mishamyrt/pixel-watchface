2eaa372a05bd06cd182019c716a7827e0d0ff7e0
mishamyrt <misha@myrt.co>
2022-02-10 00:29:13 +0400

    Improve colors

--- src/modules/color.ts
+++ src/modules/color.ts
@@ -1,27 +1,70 @@
 import convert from 'color-convert'
 import { HSL } from 'color-convert/conversions'
 
+function range (value: number, down: number, up: number) {
+  return down + ((up - down) * (value / 100))
+}
+
+// 70% 70-80
+// 80 - 70 (10)
+
 function background (color: HSL): string {
   return convert.hsl.hex([
     color[0],
-    Math.max(color[1] - 45, 10),
-    5
+    range(color[1], 30, 40),
+    range(color[2], 5, 6)
   ])
 }
 
 function mutedText (color: HSL): string {
   return convert.hsl.hex([
     color[0],
-    Math.max(color[1] - 15, 60),
-    Math.min(color[2] + 30, 90)
+    range(color[1], 50, 60),
+    range(color[2], 70, 85)
   ])
 }
 
 function headingText (color: HSL): string {
   return convert.hsl.hex([
     color[0],
-    Math.max(color[1] - 30, 20),
-    Math.min(color[2] + 40, 90)
+    range(color[1], 40, 60),
+    range(color[2], 75, 90)
+  ])
+}
+
+function card (color: HSL): string {
+  return convert.hsl.hex([
+    color[0],
+    range(color[1], 7, 15),
+    range(color[2], 8, 15)
+  ])
+}
+
+function text (color: HSL): string {
+  return convert.hsl.hex([
+    color[0],
+    range(color[1], 55, 65),
+    range(color[2], 80, 90)
+  ])
+}
+
+function complementary (color: HSL): string {
+  let hue = color[0] - 180
+  if (hue < 0) {
+    hue += 360
+  }
+  return convert.hsl.hex([
+    hue,
+    range(color[1], 50, 60),
+    range(color[1], 70, 85)
+  ])
+}
+
+function input (color: HSL): string {
+  return convert.hsl.hex([
+    color[0],
+    range(color[1], 10, 20),
+    range(color[2], 18, 25)
   ])
 }
 
@@ -30,6 +73,10 @@ export function generatePallete (color: string): Record<string, string> {
   return {
     background: '#' + background(hsl),
     muted: '#' + mutedText(hsl),
-    heading: '#' + headingText(hsl)
+    heading: '#' + headingText(hsl),
+    card: '#' + card(hsl),
+    text: '#' + text(hsl),
+    complementary: '#' + complementary(hsl),
+    input: '#' + input(hsl)
   }
 }
